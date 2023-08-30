6feb4723f3ca5c4e599349a60f1044c7434fedcf
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-08-30 15:14:18 -0300

    updated header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './styles.css'
+
+
+function Header() {
+  return (
+    <header>
+      <h2>Título</h2>
+    </header>
+  );
+
+}
+
+export default Header;
